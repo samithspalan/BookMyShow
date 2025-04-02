4a0ea242fd946fa45622c336a6fd492911e15aac
samithspalan <samithpoojary12345@gmail.com>
2025-04-03 00:08:42 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Nav from "./components/Nav"
+import Footer from "./components/Footer"
+import Body from "./components/Body"
+function Atlas() {
+
+  return (
+    <>
+     <Nav/>
+     <Body/>
+     <Footer/>
+    </>
+  )
+}
+
+export  {Atlas as App}
---
+++ src/components/Nav.jsx
@@ -0,0 +1,18 @@
+//rafce
+import React from 'react'
+import "../App.css"
+function Nav() {
+  return (
+   <nav>
+    <h1>logo</h1>
+    <ul>
+        <li>Home</li>
+        <li>About </li>
+        <li>Contact us</li>
+    </ul>
+   </nav>
+  )
+}
+
+export default Nav
+
