6b9d46cf424d9193d6be7822f2cd2ba9ef0add0e
samithspalan <samithpoojary12345@gmail.com>
2025-04-04 03:30:19 +0530

    added corousel

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -11,12 +11,9 @@ import "./App.css";
 
 function Carousel() {
   const settings = {
-    arrows: true,
-    dots: true, 
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+    dots: true,
+    autoplay:true,
+    autoplaySpeed:2500
   };
 
   return (
